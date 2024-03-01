99afab34acfc289725fddc6ad05a141a7d191007
Danielle Pashayan <daniellepashayan@gmail.com>
2024-03-01 10:06:27 -0500

    added logic for files that have a dated folder dest

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import shutil
 import json
 import os
 from loguru import logger
+import datetime
 
 def move_inputs(json_data: dict, source_dir: str):
     # setup the logging
@@ -12,6 +13,29 @@ def move_inputs(json_data: dict, source_dir: str):
         logger.info(f"Moving files for {use_case}")
         file_name = use_case_data['inputs']['name']
         destination = use_case_data['inputs']['destination']
+        
+        # check if use_case_data['inputs']['date_formatting'] exists
+        if test['inputs']['date_formatting']:
+            date_formatting = test['inputs']['date_formatting'] # YYYYMMDD
+            date_formatting_dt = test['inputs']['date_formatting_dt'] # %Y%m%d
+            # extract date from file name using value from date_formatting
+            regex_search = "(\d{"+str(len(date_formatting))+"})"
+            match = re.search(regex_search, test_file)
+            if match:
+                date = match.group(0)
+                # convert date into date_formatting_dt
+                date = datetime.datetime.strptime(date, date_formatting_dt)
+                year = date.year
+                month = date.month
+                day = date.day
+                
+                # replace date in destination with date
+                destination = destination.replace("YYYY", str(year))
+                destination = destination.replace("MM", str(month).zfill(2))
+                destination = destination.replace("DD", str(day).zfill(2))
+                
+                # make the directory if it doesnt exist
+                os.makedirs(destination, exist_ok=True)
         
         files = glob(f"{source_dir}/{file_name}")
         logger.info(f"Found {len(files)} files for {use_case}")
