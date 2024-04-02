63ff90acafc2bfc5e8da75976d3cbedf42347469
Danielle Pashayan <daniellepashayan@gmail.com>
2024-04-02 12:46:04 -0400

    moded json files to their own folder

--- main.py
+++ main.py
@@ -14,17 +14,17 @@ if __name__ == "__main__":
     inputs_dir = r'M:\CPP-Data\Sutherland RPA\Northwell Process Automation ETM Files\GOA\Inputs'
     if len(glob(inputs_dir+ '/*')) > 0:
         # read the input file
-        with open('inputs.json', 'r') as file:
+        with open('./json_data/inputs.json', 'r') as file:
             inputs = json.load(file)
-        with open('outputs.json', 'r') as file:
+        with open('./json_data/outputs.json', 'r') as file:
             outputs = json.load(file)
-        with open('./outbound_shs.json') as file:
+        with open('./json_data/outbound_shs.json') as file:
             shs = json.load(file)
 
         # move the input files to their respective destinations
         move_inputs(inputs, inputs_dir)
-        # move_outputs(outputs, inputs_dir)
         parse_output_files(shs, inputs_dir)
+        move_outputs(outputs, inputs_dir)
     else:
         logger.critical("No files found in the inputs directory")
     
