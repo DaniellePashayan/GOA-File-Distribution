b69cf779e316fef83be6341384056682440ce3df
Danielle Pashayan <daniellepashayan@gmail.com>
2024-04-04 09:08:05 -0400

    updated .bat to not close if theres an error

--- run.bat
+++ run.bat
@@ -4,5 +4,10 @@ echo "Running GOA File Distribution"
 SETLOCAL
 set FILE_PATH=%~dp0
 set SCRIPT_PATH=%FILE_PATH%main.py
+
 python -u "%SCRIPT_PATH%"
+IF ERRORLEVEL 1 (
+    echo Python script encountered an error. The error message is:
+    pause
+)
 ENDLOCAL
