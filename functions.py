0d4f723dfe6ba8d6833c168997d63f6938b897db
Danielle Pashayan <daniellepashayan@gmail.com>
2024-04-18 13:56:46 -0400

    added check for s or z drive

--- functions.py
+++ functions.py
@@ -8,6 +8,12 @@ import pandas as pd
 from zipfile import ZipFile
 
 
+def S_or_Z_drive(destination:str) -> str:
+    if os.path.exists('Z:/') and 'S:/' in destination:
+        return destination.replace('S:/','Z:/')
+    return destination
+    
+
 def move_single_file(source: str, destination: str):
     source = str(source)
     source_dir = os.path.dirname(source)
@@ -83,6 +89,7 @@ def move_inputs(data: dict, source_dir: str):
                     # change the destination only
                     destination, date = extract_date_from_file_and_replace_date_in_destination(
                         file, destination, date_formatting, date_formatting_dt)
+                    destination = S_or_Z_drive(destination)
                 move_single_file(file, destination)
 
 
@@ -105,13 +112,16 @@ def move_outputs(data: dict, source_dir: str):
                     fldr_frmt = '%m%d%Y'
                     destination = f'{destination}{date.strftime(fldr_frmt)}/'
                 destination = f'{destination}{date.strftime(date_formatting_dt)}'
+                destination = S_or_Z_drive(destination)
+                
                 if not os.path.exists(destination):
                     # unzip the file
                     with ZipFile(file, 'r') as zf:
                         zf.extractall(destination)
                     logger.success('moved files for '+ use_case)
                 # delete zip
-                # os.remove(file)
+                zip_dest = file.replace(source_dir, 'M:/CPP-Data/Sutherland RPA/Northwell Process Automation ETM Files/GOA/Inputs/moved/')
+                shutil.move(file, zip_dest)
 
 def parse_output_files(data:dict, source_dir:str):
     output_files = glob(source_dir + "/Outbound*.xlsx")
@@ -126,12 +136,16 @@ def parse_output_files(data:dict, source_dir:str):
             date_formatting_dt = '%m%d%Y'
                 
             destination,date = extract_date_from_file_and_replace_date_in_destination(output_file, use_case_data['destination'], date_formatting, date_formatting_dt, create_folder=False)
+            destination = S_or_Z_drive(destination)
             
             date_format = use_case_data['date_format'].replace("YYYY", str(date.year)).replace("MM", str(date.month).zfill(2)).replace("DD", str(date.day).zfill(2))
                 
             file_name = use_case_data['file_name'].replace(use_case_data['date_format'], date_format)
-            
+            folder = destination
+            print(folder)
             destination = destination+file_name
-            if os.path.exists(destination) and df.shape[0] > 0:
-                df.to_excel(destination, index=False)
+            print(destination)
+            if os.path.exists(folder) and df.shape[0] > 0:
+                df.to_excel(destination, index=False, sheet_name='export')
+                print('saved')
         shutil.move(output_file,output_file_dest)
