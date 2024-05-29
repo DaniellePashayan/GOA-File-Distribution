63523f7e82b18baeac76415aa88d245033ad5c1c
Danielle Pashayan <daniellepashayan@gmail.com>
2024-05-29 09:29:30 -0400

    added venv activation in bat

--- run.bat
+++ run.bat
@@ -4,8 +4,11 @@ echo "Running GOA File Distribution"
 SETLOCAL
 set FILE_PATH=%~dp0
 set SCRIPT_PATH=%FILE_PATH%main.py
+set VENV_PATH=%FILE_PATH%.venv
 
+call "%VENV_PATH%\Scripts\activate.bat"
 python -u "%SCRIPT_PATH%"
+
 IF ERRORLEVEL 1 (
     echo Python script encountered an error. The error message is:
     pause
