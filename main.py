4c2d9bf62d7d0dae1bedd8768629f7a68d834618
Danielle Pashayan <daniellepashayan@gmail.com>
2024-07-16 08:51:00 -0400

    updated logging retention and rotation

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ if __name__ == "__main__":
     try:
         os.chdir(os.path.dirname(os.path.abspath(__file__)))
         
-        logger.add('.\\logs\\local_log.log', level="INFO")
+        # logger.add('.\\logs\\local_log.log', level="INFO")
         logger.add("\\\\NT2KWB972SRV03\\SHAREDATA\\CPP-Data\\Sutherland RPA\\Northwell Process Automation ETM Files\\GOA\\Inputs\\logs\\inputs.log",
-                rotation="7 day", level="INFO")
+                rotation="1 day", level="INFO", retention="90 days", compression="zip")
         logger.add("\\\\NT2KWB972SRV03\\SHAREDATA\\CPP-Data\\Sutherland RPA\\Northwell Process Automation ETM Files\\GOA\\Inputs\\logs\\outputs.log",
-                rotation="7 day", level="INFO")
+                rotation="1 day", level="INFO",  retention="90 days", compression="zip")
 
         drives = {
             "M": r"\\NT2KWB972SRV03\SHAREDATA",
@@ -27,12 +27,12 @@ if __name__ == "__main__":
 
         # check if all drives are connected
         for drive_letter, drive_path in drives.items():
-            logger.info(f"Checking if drive {drive_letter} is connected")
-            logger.info(os.path.exists(drive_path))
+            # logger.info(f"Checking if drive {drive_letter} is connected")
+            # logger.info(os.path.exists(drive_path))
             if not os.path.exists(drive_path):
                 logger.info(f"Drive {drive_letter} is not connected")
-            else:
-                logger.info("All drives are connected")
+        if all([os.path.exists(drive_path) for drive_path in drives.values()]):
+            logger.success("All drives are connected")
 
         inputs_dir = '\\\\NT2KWB972SRV03\\SHAREDATA\\CPP-Data\\Sutherland RPA\\Northwell Process Automation ETM Files\\GOA\\Inputs'
         if len(glob(inputs_dir+ '/*')) > 0:
@@ -48,6 +48,7 @@ if __name__ == "__main__":
             move_inputs(inputs, inputs_dir)
             parse_output_files(shs, inputs_dir)
             move_outputs(outputs, inputs_dir)
+            logger.success("All files have been moved successfully")
         else:
             logger.critical("No files found in the inputs directory")
     except Exception as e:
