588ce6e08661512aa269fdf9322f7936c7cfb913
Danielle Pashayan <daniellepashayan@gmail.com>
2024-07-22 08:53:50 -0400

    remove other loggers

--- main.py
+++ main.py
@@ -12,17 +12,15 @@ if __name__ == "__main__":
         os.chdir(os.path.dirname(os.path.abspath(__file__)))
         
         # logger.add('.\\logs\\local_log.log', level="INFO")
-        logger.add("\\\\NT2KWB972SRV03\\SHAREDATA\\CPP-Data\\Sutherland RPA\\Northwell Process Automation ETM Files\\GOA\\Inputs\\logs\\inputs.log",
+        logger.add("\\\\NT2KWB972SRV03\\SHAREDATA\\CPP-Data\\Sutherland RPA\\Northwell Process Automation ETM Files\\GOA\\Inputs\\logs\\log.log",
                 rotation="1 day", level="INFO", retention="90 days", compression="zip")
-        logger.add("\\\\NT2KWB972SRV03\\SHAREDATA\\CPP-Data\\Sutherland RPA\\Northwell Process Automation ETM Files\\GOA\\Inputs\\logs\\outputs.log",
-                rotation="1 day", level="INFO",  retention="90 days", compression="zip")
 
         drives = {
-            "M": r"\\NT2KWB972SRV03\SHAREDATA",
-            "N": r"\\NASDATA204\SHAREDATA\BOT CLAIMSTATUS DATA-PHI",
-            "S": r"\\NASHCN01\SHAREDATA",
-            "Y": r"\\NASDATA201\SHAREDATA\MV-RCR01\SHARED",
-            "T": r"\\NASDATA201\SHAREDATA\NSHS-CENTRAL-LAB\SHARED\BILLING"
+            "M": "\\\\NT2KWB972SRV03\\SHAREDATA",
+            "N": "\\\\NASDATA204\\SHAREDATA\BOT CLAIMSTATUS DATA-PHI",
+            "S": "\\\\NASHCN01\\SHAREDATA",
+            "Y": "\\\\NASDATA201\\SHAREDATA\\MV-RCR01\\SHARED",
+            "T": "\\\\NASDATA201\\SHAREDATA\\NSHS-CENTRAL-LAB\\SHARED\\BILLING"
         }
 
         # check if all drives are connected
