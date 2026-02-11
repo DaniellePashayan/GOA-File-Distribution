7f2d2d0837f6cf6e31f3250df52fa1a5059183d0
Danielle Pashayan <daniellepashayan@gmail.com>
2026-02-11 11:04:19 -0500

    added more logging

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from glob import glob
 import json
 import os
 from loguru import logger
+from datetime import datetime
 
 from functions import move_inputs, move_outputs, parse_output_files, parse_epic_output_files
 
@@ -13,6 +14,12 @@ if __name__ == "__main__":
         # logger.add('.\\logs\\local_log.log', level="INFO")
         logger.add("\\\\NT2KWB972SRV03\\SHAREDATA\\CPP-Data\\Sutherland RPA\\Northwell Process Automation ETM Files\\GOA\\Inputs\\logs\\log.log",
                 rotation="1 day", level="INFO", retention="90 days", compression="zip")
+        logger.add("./logs/log.log",rotation="7 days", level="DEBUG", retention="7 days", compression="zip")
+        
+        start_time = datetime.now()
+        
+        logger.debug(f'========================================================')
+        logger.debug(f'Starting Process at: {start_time}')
 
         drives = {
             "M": "\\\\NT2KWB972SRV03\\SHAREDATA",
@@ -32,7 +39,11 @@ if __name__ == "__main__":
             logger.success("All drives are connected")
 
         inputs_dir = '\\\\NT2KWB972SRV03\\SHAREDATA\\CPP-Data\\Sutherland RPA\\Northwell Process Automation ETM Files\\GOA\\Inputs'
-        if len(glob(inputs_dir+ '/*')) > 0:
+        files = os.listdir(inputs_dir)
+        files = [file for file in files if os.path.isfile(os.path.join(inputs_dir, file)) and "~" not in file]
+        if len(files) > 0:
+            logger.debug(f"""file dump:
+                         {files}""")
             # read the input file
             with open('./json_data/inputs.json', 'r') as file:
                 inputs = json.load(file)
@@ -49,6 +60,10 @@ if __name__ == "__main__":
             parse_epic_output_files(epic_shs, inputs_dir)
             move_outputs(outputs, inputs_dir)
             logger.success("All files have been moved successfully")
+            end_time = datetime.now()
+            logger.debug(f'========================================================')
+            logger.debug(f'Ending Process at: {end_time}')
+            logger.debug(f'Total Duration: {end_time - start_time}')
         else:
             logger.critical("No files found in the inputs directory")
     except Exception as e:
